33796782f3602ba302fc25155642433955af8782
Mitch Comardo <mcomardo@pros.com>
2014-10-10 14:30:21 -0500

    reseting Fib file

--- prog2/src/main/scala/Fib.scala
+++ prog2/src/main/scala/Fib.scala
@@ -2,11 +2,6 @@ object Fib {
 
 	type ??? = Nothing
 
-	def fib(n: Int): Int = {
-		def fib(num: Int, nxt: Int, res: Int): Int = num match {
-			case 0 => res
-			case _ => fib(num-1, nxt+res, nxt)
-		}
-		fib(n, 1, 0)
-	}
+	def fib(n: Int): Int = ???
+
 }
